02849733f619bc76d4638d14a0be0be4c1db34a4
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-23 10:49:20 +0200

    print size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
+* main - Entry point
 *
-*Return: return 0 everytime
+* Return: return 0 everytime
 */
 int main(void)
 {
-       printf("Size of a char: %u bytes\n", sizeof(char));
-       printf("Size of an int: %u bytes\n", sizeof(int));
-       printf("Size of a long int: %u bytes\n", sizeof(long int));
-       printf("Size of a long long int: %u bytes\n", sizeof(long long int));
-       printf("Size of a float: %u bytes\n", sizeof(float));
+      printf("Size of a char: %u bytes\n", sizeof(char));
+      printf("Size of an int: %u bytes\n", sizeof(int));
+      printf("Size of a long int: %u bytes\n", sizeof(long int));
+      printf("Size of a long long int: %u bytes\n", sizeof(long long int));
+      printf("Size of a float: %u bytes\n", sizeof(float));
        return (0);
 }
