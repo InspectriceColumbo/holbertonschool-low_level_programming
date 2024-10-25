ccb5d5d1ef71dad1bb751038b95a553b61f59389
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-25 17:28:19 +0200

    print alphb lthenu

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,10 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	
-	putchar(letter);
+		putchar(letter);
 	
 	for (letter = 'A'; letter <= 'Z'; letter++)
-	
-	putchar('\n');
+		putchar('\n');
 	
 	return (0);
 }
