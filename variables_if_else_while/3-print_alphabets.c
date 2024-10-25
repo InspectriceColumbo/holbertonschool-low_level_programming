0d7cb85bf2976596f189c8ddbbe3a6b48dbd4055
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-25 17:31:48 +0200

    print alphb lthenu

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main -Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -12,6 +12,7 @@ int main(void)
 		putchar(letter);
 	
 	for (letter = 'A'; letter <= 'Z'; letter++)
+		
 		putchar('\n');
 	
 	return (0);
