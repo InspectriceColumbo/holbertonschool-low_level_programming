a43840a5b5f572b5b13fa16614c5cc9ca0130b38
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-27 18:48:07 +0100

    alphabet 10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 : function prints alphabet 10x
+ *
+ *Return: return 0 everytime
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	char alphabet;
+
+	while (n < 10)
+{
+	alphabet = 'a';
+	while (alphabet <= 'z')
+{
+	_putchar(alphabet);
+	alphabet++;
+}
+	_putchar('\n');
+	n++;
+}
+}
