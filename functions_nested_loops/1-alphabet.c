01c348ccd08822d03fbb40e1571c2a8d51993560
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-27 18:29:06 +0100

    alphabet putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * function print_alphabet = its name
+ *
+ * no return (void)
+ */
+void print_alphabet(void)
+{
+int(letter)
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar('\n');
+}
