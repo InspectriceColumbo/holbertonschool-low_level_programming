bcf8ece620c3dc063de124766e726758c654e010
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-27 21:51:37 +0100

    computes absval of an int

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an int
+ * n - Integer tb computed
+ *
+ * return: the absolute value of the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
