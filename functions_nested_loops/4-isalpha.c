d49af1d4f89a30241970d98773a9e77c7c49b7c8
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-27 21:17:53 +0100

    alphab char

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic chars
+ * c - char to check
+ *
+ * Return: 1 if char is letter, lc or uc, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
