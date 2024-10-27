ee0ea02d567fa2ab2212a58dc44c396d81c54137
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-27 20:58:21 +0100

    check if char r lc

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,19 +1,13 @@
 #include "main.h"
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _islower - function that checks if char r lc
+ * c - char to check
+ * Return: 1 = lc char or 0 = uc char
  */
-int main(void)
+int _islower(int c)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
