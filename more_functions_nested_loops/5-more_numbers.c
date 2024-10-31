1c6b6b391f53872646032a61a0842c6fb5084252
InspectriceColumbo <9949@holbertonstudents.com>
2024-10-31 11:21:57 +0100

    10x numberz

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers(void) - function prints numbers from 0 to 14 ten times
+ *
+ * return: (void)
+ */
+void more_numbers(void)
+{
+	int n = 0;
+	int number;
+
+	while (n < 10)
+{
+	number = 1;
+	while (number <= 14)
+{
+	_putchar(number);
+	number++;
+}
+	_putchar('\n');
+	n++;
+}
+}
