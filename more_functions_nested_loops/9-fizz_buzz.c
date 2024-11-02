21ed5bde9e18012af3b9b777d95a33a810ec9568
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-02 20:26:53 +0100

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 	for (number = 1; number <= 100; number++)
 	{
 		if ((number % 3) == 0 && (number % 5) == 0)
-			printf("Fizz Buzz");
+			printf("FizzBuzz");
 
 		else if ((number % 3) == 0)
 			printf("Fizz");
