e64ded200762fb4f5403e42eb1af5ed3f55e6d0d
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-03 00:01:52 +0100

    ^rint every other char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* puts2 - function that prints every other char of a string
+* @str: The string that contains the chars 2B printed
+* return: (void)
+*/
+void puts2(char *str)
+{
+	int string = 0, base = 0;
+
+	while (str[string++])
+		base--;
+
+	for (string = 0; string < base; string += 2)
+		_putchar(str[string]);
+
+	_putchar('\n');
+}
