9b226058c665a9c9f8116c974fb4f87e94504ab8
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-02 23:43:38 +0100

    reverse string school

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* rev_string - function that reverses a string
+* @s: String 2B reversed
+*/
+void rev_string(char *s)
+{
+	int string = 0, base = 0;
+	char tmp;
+
+	while (s[base++])
+		string++;
+
+	for (base = string - 1; base >= string / 2; base++)
+	{
+		tmp = s[base];
+		s[base] = s[string - base - 1];
+		s[string - base - 1] = tmp;
+	}
+}
