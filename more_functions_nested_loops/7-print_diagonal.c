b6189745035b8fc479d4f0349a513b0cbd6a1514
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-02 20:24:02 +0100

    print diago

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,23 +2,24 @@
 /**
  * print_diagonal - function that prints a diagonal line
  * @n: number of time to print the character \
+ *
  */
+
 void print_diagonal(int n)
 {
 int diag, espace;
-
-	if (n > 0)
+if (n > 0)
 {
-	for (diag = 0; diag < n ; diag++)
+for (diag = 0; diag < n ; diag++)
 {
-	for (espace = 0; espace < diag; espace++)
-	_putchar (' ');
-	_putchar('\\');
+for (espace = 0; espace < diag; espace++)
+_putchar (' ');
+_putchar('\\');
 
-	if (diag == n - 1)
-	continue;
-	_putchar('\n');
+if (diag == n - 1)
+continue;
+_putchar('\n');
 }
 }
-	_putchar('\n');
+_putchar('\n');
 }
