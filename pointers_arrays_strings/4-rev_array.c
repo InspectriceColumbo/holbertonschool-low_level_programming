f1e1ca6455aaa093e000678a7e8a7c902efe6d94
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-04 17:34:25 +0100

    rev array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i;
-    int tmp;
+int i;
+int tmp;
 
-    for (i = 0; i < n / 2; i++) 
-    {
-        tmp = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = tmp;
-    }
+for (i = 0; i < n / 2; i++) 
+{
+tmp = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = tmp;
 }
-
+}
