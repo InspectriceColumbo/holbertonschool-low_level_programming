13658812a3a991b077c7c3072a1d365e3805d86a
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-04 14:29:20 +0100

    100 atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _atoi - function that converts a string to an int
+* @s: string 2B converted
+* return: the int value that is the result of the converted string 
+*/
+int _atoi(char *s)
+{
+	int signbf = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			signbf *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} 
+	while (*s++);
+
+	return (num * signbf);
+}
