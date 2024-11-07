8de2a2d247cfb0a1d80ee38015b83a1da5af425e
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-07 17:20:09 +0100

    first memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _memset - fills the first n bytes of the memoryarea pointed to by @s with b
+ * @s: pointer to the block of memory area
  * @b : the constant byte (the value to set in each byte of the memory block)
  * @n : the number of bytes of the memory area to fill with the value @b
  * Return: the pointer to @s (i.e the memory area, fno called 'memar')
