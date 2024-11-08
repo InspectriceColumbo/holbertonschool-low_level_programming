47f8f418fda8c25595d1a0e24eb414d415846993
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-08 17:55:14 +0100

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,9 +2,9 @@
 /**
  * factorial -searches for the factorial of a number
  * @n: the number of which we want to find the factorial
- * A factorial is the product of all the natural numbers less than or equal to @n
+ *A factorial is the product of all the natural numbz less than or equal to @n
  * The factorial of a positive number n is given by: (n!) = 1 * 2 * 3 * 4....n.
- * If input @n= 0, i.e 0!=1. Factorial of a negative number does not exist. 
+ * If input @n= 0, i.e 0!=1. Factorial of a negative number does not exist.
  * Return: the @n factorial, and if n<0 return -1 to indicate an error
  */
 
