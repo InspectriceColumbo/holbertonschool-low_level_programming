c412be33697e2071a55db09787f11f1afbc8c584
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-08 22:04:40 +0100

    prime number or not

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -17,3 +17,15 @@ int is_divisible(int num, int div)
 	return (is_divisible(num, div + 1));
 }
 
+/**
+ * is_prime_number - verifies that a number is a prime number
+ * @n: The number to verify
+ * Return: If the number is not prime: 0, if the number is prime return 1
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_divisible(n, 2));
+}
