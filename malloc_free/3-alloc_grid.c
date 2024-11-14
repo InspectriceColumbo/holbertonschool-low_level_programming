f45d272453ef2bc9d3b18816632b9d7213e183e6
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-14 16:24:06 +0100

    alloc grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,3 +11,26 @@
 */
 int **alloc_grid(int width, int height)
 {
+	int **grid;
+	int a, b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = (int **) malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (a = 0; a < height; a++)
+	{
+		grid[a] = (int *) malloc(width * sizeof(int));
+		if (grid[a] == NULL)
+		{
+			for (b = 0; b < a; b++)
+				free(grid[b]);
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
