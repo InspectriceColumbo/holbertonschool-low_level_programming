1cac9e917851d48b79c693be1d1eaeef212ca1b0
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-22 16:29:55 +0100

    variadic print numbz

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,8 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(nargs, n);
 
-	if (separator == NULL)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nargs, int));
