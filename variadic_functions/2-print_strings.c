290aa98bc1bbc26e773b0d88700935f09ea9fc94
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-22 16:54:17 +0100

    print strgs var

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", strg);
 		if (strg == NULL)
 			printf("(nil)");
-		if (separator != NULL && i != (n - 1))
+		else if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	va_end(nargs);
