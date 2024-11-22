d51c3f6b1c27cdfcc9ff38a9e34b058fbbcfffae
InspectriceColumbo <9949@holbertonstudents.com>
2024-11-22 20:40:12 +0100

    calculs pofinis

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Performs simple operations
+ * @argc: Number of arguments 2B passed
+ * @argv: Pointer to the array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*calcul)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = 
+	num2 =
+	calcul =
+
+	if
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if
+	{
+		printf("Error\n");
+		exit(100);
+	}
